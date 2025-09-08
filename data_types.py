7987c8ba3cbf7408ab5a2e1fc4ebbba94e2a46c0
Kevin Mahon <kevin.mahon@gmail.com>
2025-09-08 17:15:17 -0500

    More Python

---
+++ data_types.py
@@ -0,0 +1,20 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+print("Literal:", 27)
+
+# Integer
+y = -19
+print("Integer:", y)
+
+# Float
+z = 19.19
+print("Float:", z)
+
+# String
+name = "Kevin"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
