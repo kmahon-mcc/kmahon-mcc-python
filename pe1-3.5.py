6f78c5553e17aa6c2eb5cf799315567fd10b27da
Kevin Mahon <kevin.mahon@gmail.com>
2025-09-23 12:42:01 -0500

    New python file

---
+++ pe1-3.5.py
@@ -0,0 +1,20 @@
+"""
+Prompt the user to enter five names.
+Store the names in a list.
+Sort the list using the Bubble Sort algorithm.
+Reverse the sorted list using a Python list method.
+Print both the sorted and reversed lists.
+"""
+names = []
+for i in range(5):
+    names.append(input(f"Please enter name {i+1}: "))
+swapped = True
+while swapped:
+    swapped = False
+    for i in range(len(names) - 1):
+        if names[i] > names[i + 1]:
+            names[i], names[i + 1] = names[i + 1], names[i]
+            swapped = True
+print(names)
+names.reverse()
+print(names)
